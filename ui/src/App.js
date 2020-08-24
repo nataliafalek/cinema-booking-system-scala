c185aefc8ec5f1c5e1d4f0378c12a9ae027e3f9e
Nati <faleknatalia@gmail.com>
2020-08-24 17:52:46 +0200

    add ui

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+import AppDrawer from './AppDrawer'
+
+class App extends Component {
+render() {
+  return (
+    <div className="App">
+    <AppDrawer/>
+    </div>
+  );
+}
+
+}
+
+export default App;
---
+++ ui/src/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Toolbar from "@material-ui/core/Toolbar";
+import useStyles from "./material-styles/useStyles";
+
+export default function Movies() {
+    const classes = useStyles();
+
+    return (
+        <main className={classes.content}>
+            <Toolbar/>
+            MOVIES
+        </main>
+    )
+}
